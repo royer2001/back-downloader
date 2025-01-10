87b72253977831589ed98527c43d9bc5ab74ec66
royer <royercfm@gmail.com>
2025-01-09 20:31:00 -0500

    allow hosts

--- downloaderback/settings.py
+++ downloaderback/settings.py
@@ -138,12 +138,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    'https://front-downloader.vercel.app/'
+    'https://front-downloader.vercel.app/',
 ]
 
 ALLOWED_HOSTS = [
     'localhost',
-    '*'
+    'https://front-downloader.vercel.app/',
+    '*',
 ]
 
 SECURE_SSL_REDIRECT = True
